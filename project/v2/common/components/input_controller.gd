29bf7aada4ff692a0456a57e253803f5795b03ba
Jacob Fraser <jakebfraser@gmail.com>
2026-05-11 22:40:19 -0400

    Fixed punch buffer. 1-2-1-2 smooth combo with easy-to-change timing

--- project/v2/common/components/input_controller.gd
+++ project/v2/common/components/input_controller.gd
@@ -3,6 +3,7 @@ class_name InputController
 
 # --- Settings ---
 @export var hold_threshold: float = 0.25
+@export var combo_time: float = 0.6
 
 # --- Movement ---
 var move_axis: float = 0.0
@@ -15,29 +16,28 @@ var press_start_time: float = 0.0
 var is_holding: bool = false
 var punch_requested: bool = false
 var punch_held: bool = false
+var combo_step: int = 0
 
 # -- Timers --
 @onready var hold_timer: Timer = $HoldTimer
+@onready var combo_timer: Timer
 
+func _ready() -> void:
+	combo_timer = Timer.new()
+	add_child(combo_timer)
+	combo_timer.one_shot = true
+	combo_timer.timeout.connect(_on_combo_timeout)
 
 func _input(event):
-	if event.is_action_pressed("ui_jump"):
-		jump_requested = true
+	if event.is_action_pressed("ui_jump"):jump_requested = true
+	if event.is_action_released("ui_jump"): jump_requested = false
 	
-	if event.is_action_released("ui_jump"):
-		jump_requested = false
-	
-	if event.is_action_pressed("ui_punch"):
-		_handle_punch_press()
-	
-	if event.is_action_released("ui_punch"):
-		_handle_punch_release()
+	if event.is_action_pressed("ui_punch"): _handle_punch_press()
+	if event.is_action_released("ui_punch"): _handle_punch_release()
 
 func _process(delta):
 	move_axis = Input.get_axis("ui_left", "ui_right")
-	if Input.is_action_just_pressed("ui_jump"):
-		jump_held = true
-		
+
 # --- Punch Handling ---
 
 func _handle_punch_press():
@@ -53,12 +53,23 @@ func _handle_punch_release():
 
 # --- Consumption (called by states) ---
 
+func _on_combo_timeout():
+	reset_combo()
+
+func reset_combo():
+	combo_step = 0
+	combo_timer.stop()
+	
 func consume_punch() -> Dictionary:
-	var punch_data = {"requested": punch_requested, "held": punch_held}
+	var punch_data = {"requested": punch_requested,"held": punch_held, "combo": combo_step}
 	if punch_requested:
 		punch_requested = false
 		hold_timer.stop()
 	return punch_data
+	
+func advance_combo():
+	combo_step += 1
+	combo_timer.start(combo_time)
 
 func consume_jump() -> bool:
 	if jump_requested:
